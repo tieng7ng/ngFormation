8edebd73e32e8e5dabe681c8fd00122d4737de3e
tieng7ng <tieng@laposte.net>
2018-12-05 10:55:59 +0100

    mise en place : databinding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { Component1Component } from './component1/component1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Component1Component
   ],
   imports: [
     BrowserModule
